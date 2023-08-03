de3a081c09425c59930d8362f2c22c8e2dc5d2af
Jesse <jjhassel@mtu.edu>
2023-08-02 23:18:25 -0400

    Updated Utility docstrings

--- utilz.cpp
+++ utilz.cpp
@@ -1,19 +1,31 @@
 #include "utilz.h"
 
+/**
+ * Strips away the file extension 
+ * @param :<chr*|__FILE__> - the file extension
+ * @return :<str|filename>
+*/
 std::string Utilz::FileName(const char* file) {
-	/* Returns a string containing the
-	 * name of the given file 
-	 */
 	std::string fileName = file;
 	size_t pos = fileName.find(".");
 	return fileName.substr(0, pos);
 }
 
+/**
+ * Truncating method to strip away file extension 
+ * @param :<int|length> - Size of return string
+ * @param :<chr*|__FILE__> - the file extension
+ * @return :<str|filename>
+*/
 std::string Utilz::FileName(int length, const char* file) {
 	std::string output = Utilz::FileName(file);
 	return Utilz::HeadString(length, output);
 }
 
+/**
+ * Formatted Timestamp for uniformity
+ * @return :<str|timestamp>
+*/
 std::string Utilz::TimeStamp() {
 	std::time_t time = std::time({});
 	char output[std::size("ddd mmm dd hh:mm:ss")];
@@ -27,11 +39,24 @@ std::string Utilz::TimeStamp() {
 	return output;
 }
 
+/**
+ * Truncates a string from the beginning of a string
+ * @param :<int|length> - Size of return string
+ * @param :<str|message> - String to be truncated
+ * @return :<str|trunc>
+*/
 std::string Utilz::HeadString(int length, std::string input) {
 	std::string output = input;
 	output.resize(length);
 	return output;
 }
+
+/**
+ * Truncates a string from the ending of a string
+ * @param :<int|length> - Size of return string
+ * @param :<str|message> - String to be truncated
+ * @return :<str|trunc>
+*/
 std::string Utilz::TailString(int length, std::string input) {
 	std::string output;
 	size_t l = input.size() - length;
@@ -39,6 +64,10 @@ std::string Utilz::TailString(int length, std::string input) {
 	return output;
 }
 
+/**
+ * Removes whitespace from string
+ * @param :<str|message> - String to be crunched
+*/
 void Utilz::Strip(std::string& input) {
 	std::string::iterator pos = std::remove(input.begin(), input.end(), ' ');
 	input.erase(pos, input.end());
--- utilz.h
+++ utilz.h
@@ -7,14 +7,12 @@
 #include <string>
 
 namespace Utilz {
-
     std::string FileName(const char *);
     std::string FileName(int, const char *);
     std::string TimeStamp();
     std::string HeadString(int, std::string);
     std::string TailString(int, std::string);
     void Strip(std::string&);
-
 }; // namespace Utilz
 
 #endif // UTILZ_H //
