ab575c6747b24facae405061775f181cf1128a96
Jesse <jjhassel@mtu.edu>
2023-08-02 22:09:01 -0400

    Improved docstrings

--- helpsuite/helper.cpp
+++ helpsuite/helper.cpp
@@ -1,13 +1,27 @@
+/**
+ * Helper Suite is meant to parse Doxygen outputs, and provide
+ * a commandline utility for quick reference while developing 
+ * or debugging.
+*/
 #include "helper.h"
 
+/**
+ * Default Constructor
+*/
 HelpSuite::HelpSuite() {
     cm = ConfigManager::GetInstance();
     log = Logger::GetInstance();
     log->named_log(__FILE__, "Helper Loaded!");
 }
 
+/**
+ * Default Deconstructor
+*/
 HelpSuite::~HelpSuite() {}
 
+/**
+ * Module Entry Point
+*/
 int main(int argc, char const *argv[]) {
     HelpSuite h;
     return 0;
