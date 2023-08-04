7c73c94c514e14b3f25fe2eb216d325952728770
Jesse Hassell <jjhassel@mtu.edu>
2023-08-04 11:04:27 -0400

    Corrected Nomeclature

--- testsuite/actors.cpp
+++ testsuite/actors.cpp
@@ -18,7 +18,7 @@ void TestActors::test_actor() {
  * Are actors getting proper base values?
 */
 void TestActors::base_attack() {
-    int cnf_atk = conf->get_attack();
+    int cnf_atk = cnf->get_attack();
     this->dummy = new Actor();
     assert(cnf_atk == this->dummy->get_attack());
     BaseCase::log->named_log(__FILE__, "Tested Base Attack Value for Actors");
@@ -28,7 +28,7 @@ void TestActors::base_attack() {
  * Are actors getting proper base values?
 */
 void TestActors::base_defense() {
-    int cnf_def = conf->get_defense();
+    int cnf_def = cnf->get_defense();
     this->dummy = new Actor();
     assertm(cnf_def == this->dummy->get_defense(), "Actor Defense Mismatch\t<--");
     BaseCase::log->named_log(__FILE__, "Tested Base Defense Value for Actors");
@@ -38,7 +38,7 @@ void TestActors::base_defense() {
  * Are actors getting proper base values?
 */
 void TestActors::base_health() {
-    int cnf_hlt = conf->get_health();
+    int cnf_hlt = cnf->get_health();
     this->dummy = new Actor();
     assert(cnf_hlt == this->dummy->get_health());
     BaseCase::log->named_log(__FILE__, "Tested Base Health Value for Actors");
