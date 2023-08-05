a46bcee65cfba5e798d655fc3de1389d0779bc86
Jesse <jjhassel@mtu.edu>
2023-08-04 22:03:30 -0400

    Added CLI Help Message

--- testsuite/testsuite.cpp
+++ testsuite/testsuite.cpp
@@ -37,12 +37,18 @@ TestSuite::~TestSuite() { }
 void print_help() { 
     Logger* log = Logger::GetInstance();
     char buf[1024];
-    sprintf(buf, "Usage: %s [NUMBER]\n", Utilz::FileName(__FILE__)); 
-    log->named_log(__FILE__, buf);
-    log->named_log(__FILE__, "\t-:1 - Test Actor Module");
-    log->named_log(__FILE__, "\t-:2 - Test Balance Module");
-    log->named_log(__FILE__, "\t-:3 - Test Combat Module");
-    log->named_log(__FILE__, "\t-:0 - Default Test to rule them all\n");
+    std::string fileName = Utilz::FileName(__FILE__);
+    sprintf(buf, "Usage: %s [NUMBER]\n", fileName.c_str()); 
+    log->raw_log(buf);
+    log->raw_log("TestSuite CLI Tool\n");
+    log->raw_log("A testing system, to give the developer/debugger" \
+        " a quality-of-life option for hunting down all my stupid." \
+        " This can be used in conjuction with the HelpSuite in order to"\
+        " maximize the benefit of the CLI Debugging Suite.\n");
+    log->raw_log("\t[1] - Test Actor Module");
+    log->raw_log("\t[2] - Test Balance Module");
+    log->raw_log("\t[3] - Test Combat Module");
+    log->raw_log("\t[0] - Default Test to rule them all\n");
     exit(-1);
 }
 
