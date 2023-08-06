bca9dbb11f1000dec8c37895bdbbd74aec55c75f
Jesse <jjhassel@mtu.edu>
2023-08-06 16:05:24 -0400

    Improved Docstrings

--- core/balance.cpp
+++ core/balance.cpp
@@ -6,14 +6,14 @@ BalanceController* BalanceController::_singleton = NULL;
 std::mutex BalanceController::_mutex;
 
 /*! 
- * \brief 		Balance Controller 
- * \details 	Like in life, so to in games do we need balance
+ * @brief 		Balance Controller 
+ * @details 	Like in life, so to in games do we need balance
  * 				In order to maintain consistency in a game, you 
  * 				need a universal scale in which you operate from. 
  * 				This module creates a mathematical scalar curve 
  * 				that all statistics can be derived from. 
  * 
- * \note		y = c^[x/(π^π)]
+ * @note		y = c^[x/(π^π)]
  */
 BalanceController::BalanceController() {
 	//! Load Configurations Objects
@@ -42,7 +42,7 @@ BalanceController::BalanceController() {
 	}
 }
 
-/**
+/*!
  *  Singleton Constructor 
 */
 BalanceController *BalanceController::GetInstance() {
@@ -53,53 +53,57 @@ BalanceController *BalanceController::GetInstance() {
     return _singleton;
 }
 
-/**
- * Scalar Function to keep the entire universe in balance
- * y = δ^(χ/[π^π])
- * \return Scaled value based on level
+/*!
+ * @brief		Scales The Universe
+ * 
+ * @details		Scalar Function to keep the entire universe 
+ *				in balance.
+ *
+ * @note		y = δ^(χ/[π^π])
+ * 
+ * @return Scaled value based on level
 */
 double BalanceController::scalar(int level) {
 	double x = level * 1.0;
 	return pow(base, (x / pow(M_PI, M_PI)));
 }
 
-/**
+/*!
  * Display the current state of the base game attributes
 */
 void BalanceController::display_state() {
 	double scl = 0.0;
-	char tmsg[2048];
 	int spn = this->span;
 	int attack = this->atk;
 	int defense = this->def;
 	
-	sprintf(tmsg, "%*s :: %*s :: %*s", 4, "Lvl", spn, "Attack", spn, "Defense");
-	log->named_log(__FILE__, tmsg);
+	sprintf(buf, "%*s :: %*s :: %*s", 4, "Lvl", spn, "Attack", spn, "Defense");
+	log->named_log(__FILE__, buf);
 	for (int i = 0; i < MAXLVL; i++) {
 		scl = this->scalar(i);
-		sprintf(tmsg, "%*d :: %*.4f :: %*.4f", 4, i, spn, (scl*attack), spn, (scl*defense));
-		log->named_log(__FILE__, tmsg);
+		sprintf(buf, "%*d :: %*.4f :: %*.4f", 4, i, spn, (scl*attack), spn, (scl*defense));
+		log->named_log(__FILE__, buf);
 	}
 }
 
-/**
+/*!
  * Helper Function: Base Scalar Value 
- * \return Returns Base Scalar Value
+ * @return Returns Base Scalar Value
 */
 double BalanceController::get_base() { return base; }
 
-/**
+/*!
  * Helper Function: Game Difficulty 
- * \return Returns difficulty level
+ * @return Returns difficulty level
 */
 Hardness BalanceController::get_difficulty() { return this->DIF; }
 
-/**
+/*!
  * Helper Hook used in CLI Help System
 */
 void BalanceController::_help() { }
 
-/**
+/*!
  * Default Deconstructor
 */
 BalanceController::~BalanceController() {}
