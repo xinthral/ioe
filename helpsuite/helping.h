c38d72cd09f40119d0f94d2f03c72c27f7a99abc
Jesse <jjhassel@mtu.edu>
2023-08-06 22:16:46 -0400

    Added Helping Inception boilerplate

---
+++ helpsuite/helping.h
@@ -0,0 +1,12 @@
+#ifndef HELPHELPSUITE_H
+#define HELPHELPSUITE_H
+
+#include "basehelp.h"
+
+class HelpHelping : public BaseHelp {
+protected:
+private:
+public:
+};
+
+#endif // HELPHELPSUITE_H //
