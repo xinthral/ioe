5d6ff325fb8ed5be8f2d06e4f226ce5a3801e24a
Jesse <jjhassel@mtu.edu>
2023-08-10 07:28:49 -0400

    Boiler Plate Class

--- core/battle.cpp
+++ core/battle.cpp
@@ -3,3 +3,34 @@
  * @brief   Interweaving combat events
  */
 #include "battle.h"
+
+/*! 
+ * @brief   Default Constructor
+*/
+Battle::Battle() {}
+
+/*! 
+ * @overload
+ * @brief   Player v Team Constructor
+*/
+Battle::Battle(int size, Player* player, std::vector<Toon*>& team) {
+    Toon* t, * v;
+    Combat* cc;
+    while (team.size() > 1) {
+		t = team[team.size() - 1];
+		team.pop_back();
+		v = team[team.size() - 1];
+		team.pop_back();
+		cc = new Combat(*t, *v);
+		cc->begin_combat();
+	}
+
+	cc = new Combat(*player, *t);
+	cc->begin_combat();
+	
+}
+
+/*! 
+ * @brief   Default Deconstructor
+*/
+Battle::~Battle() {}
--- core/battle.h
+++ core/battle.h
@@ -1,4 +1,17 @@
 #ifndef BATTLE_H
 #define BATTLE_H
 
+#include "combat.h"
+#include "player.h"
+#include "toon.h"
+
+class Battle {
+protected:
+private:
+public:
+    Battle();
+    Battle(int, Player*, std::vector<Toon*>&);
+    ~Battle();
+};
+
 #endif // BATTLE_H //
