ae554c09571263e9ace61e4ad4335bf6ca8c3cd4
Jesse <jjhassel@mtu.edu>
2023-08-10 07:29:33 -0400

    Restructured Constructors

--- core/player.cpp
+++ core/player.cpp
@@ -11,20 +11,36 @@
  * @details If no values are provided, then default
  *          values are initialized as (1, 1, 1).
 */
-Player::Player() : Player(1, 1, 1) {}
+Player::Player() {
+    log = Logger::GetInstance();
+    sprintf(buf, "Level %d Player initiated.", level);
+    log->named_log(__FILE__, buf);
+}
 
 /*!
  * @overload
- * @brief   Constructor Initializor
+ * @brief   Level Intialized Constructor
  * @param[in] level - Level of the Player
  * @param[in] power - Cummulative Power of Player
  * @param[in] block - Defense of the Player
 */
-Player::Player(int level, int power, int block) :
-    level(level), power(power), block(block) {
-    sprintf(buf, "Level %d Player initiated.", level);
-    log = Logger::GetInstance();
-    log->named_log(__FILE__, buf);
+Player::Player(int level, int power, int block) : Player() { 
+    this->level = level;
+    this->power = power;
+    this->block = block;
+}
+
+/*!
+ * @overload
+ * @brief   Constructor Initializor
+ * @param[in] name  - Name of the Player
+ * @param[in] level - Level of the Player
+ * @param[in] power - Cummulative Power of Player
+ * @param[in] block - Defense of the Player
+*/
+Player::Player(std::string name, int level, int power, int block) : 
+    Player(level, power, block) {
+        this->name = name;
 }
 
 /*!
--- core/player.h
+++ core/player.h
@@ -4,16 +4,18 @@
 #include "actor.h"
 #include "logger.h"
 
-class Player : private Actor {
+class Player : public Actor {
 private:
     Logger* log;
     char buf[128];
-    int level;
-    int power;
-    int block;
+    int block   = 1;
+    int health  = 1;
+    int level   = 1;
+    int power   = 1;
 public:
     Player();
     Player(int, int, int);
+    Player(std::string, int, int, int);
     void _help();
     ~Player();
 };
