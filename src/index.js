895b84eb8fac26ae3e275d440f5796d1c9ba81fb
Jesse <jjhassel@mtu.edu>
2023-08-19 21:55:45 -0400

    Testing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GameComponent from './engine/core/core';
-import { RouterProvider } from 'react-router-dom';
-import router from './pages/routes'
-import Root from './pages/root/root';
-import { Users } from './UI/component/users';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      {/* <Root /> */}
-      <Users />
-    </RouterProvider>
+    <App />
   </React.StrictMode>
 );
 
--- src/pages/root/root.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Root from './root';
-
-test('renders learn react link', () => {
-  render(<Root />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
