0062ce723b8556e7299cade8ded8e4f6ad46e747
Jesse <jjhassel@mtu.edu>
2023-09-03 00:01:51 -0400

    Some consistency in design

--- backend/db/queries/createToonTable.js
+++ backend/db/queries/createToonTable.js
@@ -1,13 +1,15 @@
 const tableNames = require('../structures/tableNames');
 const createQueries = require('../structures/createQueries');
 
-/*! Toons Table */
-tableNames.push('toons');
-var createToonQuery = `CREATE TABLE IF NOT EXISTS ${tableNames[1]} (` +
-    'uid VARCHAR(32) PRIMARY KEY NOT NULL, ' +
-    'name VARCHAR(16) NOT NULL, ' +
-    'level INTEGER DEFAULT(0), ' +
-    'ownedBy VARCHAR(32), ' +
-    `FOREIGN KEY (ownedBy) REFERENCES ${tableNames[0]}(uid)` +
-    ');'
-createQueries.push(createToonQuery)
+export default createToons = () => {
+    /*! Toons Table */
+    tableNames.push('toons');
+    var createToonQuery = `CREATE TABLE IF NOT EXISTS ${tableNames[1]} (` +
+        'uid VARCHAR(32) PRIMARY KEY NOT NULL, ' +
+        'name VARCHAR(16) NOT NULL, ' +
+        'level INTEGER DEFAULT(0), ' +
+        'ownedBy VARCHAR(32), ' +
+        `FOREIGN KEY (ownedBy) REFERENCES ${tableNames[0]}(uid)` +
+        ');'
+    createQueries.push(createToonQuery)
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainPage from './pages/main';
 import TodoLanding from './pages/todos/landing';
-import Root from './routes/root';
+import SignUp from './pages/login/landing';
 import PlayerLanding from './pages/players/landing';
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
@@ -10,8 +10,8 @@ const router = createBrowserRouter([
 	{path: "/", element: <MainPage />,},
 	{path: "/todos", element: <TodoLanding />},
 	{path: "/players", element: <PlayerLanding />},
-	{path: "/root", element: <Root />},
-])
+	{path: "/signup", element: <SignUp />},
+]);
 
 const App = () => {
 	return (<RouterProvider router={router} />);
