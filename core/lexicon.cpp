086886de6e0cbb668a63679e1fcd2b14eba30189
Jesse <jjhassel@mtu.edu>
2023-12-29 21:26:00 -0500

    Feature Addition: Lexicon

---
+++ core/lexicon.cpp
@@ -0,0 +1,16 @@
+/*!
+ * @class   Lexicon lexicon.cpp lexicon.h
+ * @brief   The Lexicon will handle ciphers, names, word generations and hold
+ *          the names generated for lookup
+*/
+#include "lexicon.h"
+
+/*!
+ * @brief   Default Constructor
+*/
+Lexicon::Lexicon() { }
+
+/*!
+ * @brief   Default Deconstructor
+*/
+Lexicon::~Lexicon() { }
---
+++ core/lexicon.h
@@ -0,0 +1,12 @@
+#ifndef LEXICON_H
+#define LEXICON_H
+
+class Lexicon {
+protected:
+private:
+public:
+  Lexicon();
+  ~Lexicon();
+};
+
+#endif // LEXICON_H //
