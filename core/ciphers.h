7a31db21cf6883a0234f3b7d0bf0d8c640996567
Jesse <jjhassel@mtu.edu>
2024-01-09 00:57:09 -0500

    Docstring refactoring

--- core/ciphers.h
+++ core/ciphers.h
@@ -3,24 +3,56 @@
 
 #include "lexicon.h"
 
+/*!
+ * @class   xCiphers ciphers.cpp ciphers.h
+ * @brief   The cipher suite will handle logical lexiconal shifting
+ *          and word translations
+*/
 class xCiphers : public Lexicon {
 protected:
+  /*!
+   * @brief   Locates the index of the given letter in the lexigraph 
+   * @param[in] letter - Character to be looked up
+  */
+  int getIndex(char);
+private:
   std::vector<std::string> shadowMatrix;
   std::vector<std::string> outputMatrix;
   const char *keyArray;
   int rows;
   int cols;
   int keySize;
-private:
-  int getIndex(char);
 public:
+  /*!
+   * @brief   Default Constructor
+  */
   xCiphers();
+
+  /*!
+   * @brief   Overloaded Constructor
+   * @param[in] code - Character to start the cipher
+   * @param[in] key  - Salt for the cipher
+  */
   xCiphers(char,std::string);
+
+  /*!
+   * @brief   Generates an N x N matrix where N := Number of elements
+   *          in the lexigraph.
+  */
   void generateMatrix(char);
+
+  /*!
+   * @brief   Gets the size of the matrix
+   * @return  Size of the matrix
+  */
   int getMatrixSize();
   std::string encode(std::string);
   std::string decode(std::string);
   void displayMatrix(bool);
+
+  /*!
+   * @brief   Default Deconstructor
+  */
   ~xCiphers();
 };
 
