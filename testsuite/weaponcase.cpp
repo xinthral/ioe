30fd2b24bcfe6b9745137a46435c72b23b709e6f
Jesse <jjhassel@mtu.edu>
2025-02-03 21:31:20 -0500

    dos2unix conversion

--- testsuite/weaponcase.cpp
+++ testsuite/weaponcase.cpp
@@ -19,10 +19,13 @@ void TestWeapon::test_all() {
 
 void TestWeapon::test_weapon_type() {
   PROFILE_FUNCTION();
+  ItemType staff = ItemType::STAFF;
   ItemType sword = ItemType::SWORD;
   Weapon * wep = new Weapon();
+  wep->set_type(staff);
+  assertm(wep->get_type() == staff, "Weapon type mismatch!\n");
   wep->set_type(sword);
-  assertm(wep->get_type() == sword, "Weapon type mismatch!\n");
+  assertm(wep->get_type() == sword, "Weapon retype mismatch!\n");
   sprintf(buf, "%s [%s] %s", msgHead, "weapon type check", msgTail);
   BaseCase::log->named_log(__FILENAME__, buf);
 }
