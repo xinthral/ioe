ef0da008712e201341026c2e267b822a6f2aff6a
Jesse <jjhassel@mtu.edu>
2026-04-20 00:00:48 -0400

    preAlpha v0.5.3: Updated Battle docstrings

--- core/battle.cpp
+++ core/battle.cpp
@@ -23,7 +23,7 @@ Battle::Battle() {
 }
 
 /*!
- * @brief   Player v Team Constructor
+ * @brief   Return the singleton Battle instance, creating it on first call
 */
 Battle* Battle::GetInstance() {
   //! Acquire Instance Mutex
@@ -50,20 +50,35 @@ void Battle::doCycleWork(bool &isPendingWork) {
   }
 }
 
+/*!
+ * @brief   Start a Toon vs Toon combat session
+ * @param[in] t1  First NPC combatant
+ * @param[in] t2  Second NPC combatant
+*/
 void Battle::startEVE(Toon *t1, Toon *t2) {
   this->combat = new Combat(t1, t2);
 }
 
+/*!
+ * @brief   Start a Player vs Toon combat session
+ * @param[in] player  Player combatant
+ * @param[in] t       NPC combatant
+*/
 void Battle::startPVE(Player *player, Toon *t) {
   this->combat = new Combat(player, t);
 }
 
+/*!
+ * @brief   Start a Player vs Player combat session
+ * @param[in] player1  First Player combatant
+ * @param[in] player2  Second Player combatant
+*/
 void Battle::startPVP(Player *player1, Player *player2) {
   this->combat = new Combat(player1, player2);
 }
 
 /*!
- * @todo    Helper Hook used in CLI Help System
+ * @brief   Print a description of the Battle module to the log
 */
 void Battle::_help() {
   std::string helpline = "\nBattle Helpline!"
